907a7ae7a6d1ba428625cd3d3d93e6cfa203ec47
Kinjal62 <khvce62@gmail.com>
2019-03-28 12:48:08 +0530

    loader with content using jquery

--- js/custom.js
+++ js/custom.js
@@ -129,3 +129,14 @@ $(document).ready(function(){
 		$('.loader').fadeout('slow');
 	})
 })
+// loader witn content
+var myVar;
+
+function myFunction() {
+	myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+	document.getElementById("loader").style.display = "none";
+	document.getElementById("myDiv").style.display = "block";
+}
